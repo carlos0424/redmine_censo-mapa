4c2b1b23bee252b203f7226588eec9f9d4276f10
Carlos Arbelaez <tu_email@example.com>
2024-12-16 01:24:40 -0500

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Configuración de rutas para el plugin
 RedmineApp::Application.routes.draw do
-    get 'projects/:project_id/censo_mapa', to: 'censo_mapa#index'
+    get 'projects/:project_id/censo-mapa', to: 'censo_mapa#index'
   end
